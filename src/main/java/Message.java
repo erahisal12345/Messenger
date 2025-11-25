e2e0a0e33f82248cece0c16d4ac9ed1df3a5f60a
Olina <alina.usupova2007@gmail.com>
2025-11-25 20:48:25 +0500

    Methods for creating lists of sent and received messages

---
+++ src/main/java/Message.java
@@ -0,0 +1,17 @@
+public class Message {
+    private User sender;
+    private User receiver;
+    private String text;
+    private String time;
+
+    public Message(User sender, User receiver, String text, String time) {
+        this.sender = sender;
+        this.receiver = receiver;
+        this.text = text;
+        this.time = time;
+    }
+
+
+
+
+}
--- src/main/java/User.java
+++ src/main/java/User.java
@@ -2,6 +2,7 @@ public class User {
     private String name;
     private String[] sentMessages;
     private String[] receivedMessages;
+    private int cnt = 0;
 
     public User(String name) {
         this.name = name;
@@ -9,8 +10,22 @@ public class User {
         this.receivedMessages = new String[100];
     }
 
+    public void sendMessage(User sender, User receiver, String text, String time) {
+        Message message = new Message(sender, receiver, text, time);
+        receiver.receivedMessages[cnt] = text;
+        sender.sentMessages[cnt] = text;
+        cnt ++;
+    }
+
     public String getName() { return name; }
     public String[] getSentMessages() { return sentMessages; }
     public String[] getReceivedMessages() { return receivedMessages; }
 
+    public User createUser(String name) {
+        User user = new User(name);
+    }
+
+    public User[] users(User user) {
+
+    }
 }
