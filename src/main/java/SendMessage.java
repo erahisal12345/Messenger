567be2d5ffdbef57c6656e04f53dd4d701f0413b
Olina <alina.usupova2007@gmail.com>
2025-11-25 23:01:30 +0500

    Addin methods for certain receivers in SendMessages class

--- src/main/java/SendMessage.java
+++ src/main/java/SendMessage.java
@@ -1,12 +1,24 @@
 public class SendMessage {
-    private String[] historyMessage;
+    private String[] historyMessage = new String[100];
     private int cnt = 0;
 
     public String[] getHistoryMessage() { return historyMessage; }
 
     public void sendMessage(User sender, User receiver, String text, String time) {
         Message message = new Message(sender, receiver, text, time);
-        historyMessage[cnt] = text;
-        cnt ++;
+        historyMessage[cnt ++] = text;
+        System.out.println("Отправлено личное сообщение от " + sender.getName() + " " + sender.getID() + " для " + receiver.getName() + " " + receiver.getID());
+    }
+
+    public void sendMessage(User sender, Group group, String text, String time) {
+        Message message = new Message(sender, group, text, time);
+        historyMessage[cnt ++] = text;
+        System.out.println("Отправлено сообщение в группу");
+    }
+
+    public void sendMessage(User sender, Channel channel, String text, String time) {
+        Message message = new Message(sender, channel, text, time);
+        historyMessage[cnt ++] = text;
+        System.out.println("Отправлен пост в канал");
     }
 }
--- src/main/java/User.java
+++ src/main/java/User.java
@@ -7,6 +7,7 @@ public class User {
     public User(String name) {
         this.name = name;
         this.ID = ID;
+
     }
 
 
