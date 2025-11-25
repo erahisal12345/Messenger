96c88c9e22a35e9e4246a28c774c0c3d4223e538
Olina <alina.usupova2007@gmail.com>
2025-11-25 20:12:38 +0500

    Creating Message class

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,2 +1,16 @@
 public class User {
+    private String name;
+    private String[] sentMessages;
+    private String[] receivedMessages;
+
+    public User(String name) {
+        this.name = name;
+        this.sentMessages = new String[100];
+        this.receivedMessages = new String[100];
+    }
+
+    public String getName() { return name; }
+    public String[] getSentMessages() { return sentMessages; }
+    public String[] getReceivedMessages() { return receivedMessages; }
+
 }
