b2866aabb6f64303b502989be2e63ba96f64c605
Olina <alina.usupova2007@gmail.com>
2025-11-26 22:22:44 +0500

    Editing classes Messages, User, SendMessage

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -2,22 +2,26 @@ public class User {
     private String name;
     private int ID = 0;
     private User[] users = new User[100];
-    private int cnt = 0;
 
     public User(String name) {
         this.name = name;
-        this.ID = ID;
+    }
 
+    public User() {
+        this.name = "default";
     }
 
 
     public String getName() { return name; }
     public int getID() { return ID; };
 
-    public void createUser(String name) {
+    public User createUser(String name) {
         User user = new User(name);
+        user.ID = ID;
         users[ID] = user;
         ID ++;
+        return user;
     }
 
+    public User[] getUsers() { return users; }
 }
