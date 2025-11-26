755576c190139f934e4e649c3664b8bdf8742ba6
karina <erahisal12345@gmail.com>
2025-11-26 20:04:54 +0300

    Добавлены классы Group и Channel

--- src/main/java/Channel.java
+++ src/main/java/Channel.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
-
 public class Channel {
+    private String name;
+    private User admin;
+    private User[] subs = new User[100];
+    private int subsCount;
+    private User userChannel;
+
+    public Channel(String name, User admin) {
+        this.name = name;
+        this.admin = admin;
+        this.subs[0] = admin;
+        subsCount++;
+        userChannel = new User(name);
+    }
+    public boolean containsMember(User suchUser) {
+        for (int i = 0; i < subs.length; ++i) {
+            if (subs[i] == suchUser) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public void addSub(User user) {
+        if (!containsMember(user)) {
+            subs[subsCount++] = user;
+            System.out.println("Пользователь " + user.getName() + "подписался на канал" + name);
+        }
+    }
+    public String getName() { return name; }
+    public User getAdmin() {
+        return admin;
+    }
+    public User getUser () { return userChannel; }
 }
